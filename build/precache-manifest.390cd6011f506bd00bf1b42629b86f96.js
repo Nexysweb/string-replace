2d30dbe2fb5e37de07adfe60e5103123b574dede
John B <john@nexys.ch>
2019-11-22 15:27:42 +0100

    add ui

---
+++ build/precache-manifest.390cd6011f506bd00bf1b42629b86f96.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1ac598f7e3ed2d5db483e9dfadacd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c3bdb9f499a772fe34d",
+    "url": "/static/js/2.09d653a8.chunk.js"
+  },
+  {
+    "revision": "b32fbffd3dc45d1c615c",
+    "url": "/static/js/main.cb7a26d9.chunk.js"
+  },
+  {
+    "revision": "3886ed00308341eac789",
+    "url": "/static/js/runtime-main.39cd371c.js"
+  }
+]);
