ec9033cd84ba1b81d86191b60ce67a50a6b4ffbc
John B <john@nexys.ch>
2020-01-06 08:29:40 +0100

    new build

---
+++ docs/precache-manifest.52cb5a86dbf73a7a00b19c81b8f4425e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e05a0e469e8160fa129c7ca14566d29",
+    "url": "/string-replace/index.html"
+  },
+  {
+    "revision": "6f82c40c02c88f8a3e87",
+    "url": "/string-replace/static/js/2.4bacaf24.chunk.js"
+  },
+  {
+    "revision": "4f34c37463fa4a2f4f5c",
+    "url": "/string-replace/static/js/main.622543b8.chunk.js"
+  },
+  {
+    "revision": "4142cdbe781c1fbcde25",
+    "url": "/string-replace/static/js/runtime-main.84b53287.js"
+  }
+]);
