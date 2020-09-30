c9659b04eaafbf3d74a6ba938373dfbc5cf38104
John B <johan@nexys.ch>
2020-09-30 20:53:35 +0200

    Update sql.ts

--- src/lib/sql.ts
+++ src/lib/sql.ts
@@ -24,3 +24,29 @@ const sqlOutToArray = (content: string): (string | number | null)[][] => {
       });
   });
 };
+
+
+// transpiled (to be deleted) from https://babeljs.io/en/repl#?browsers=&build=&builtIns=false&spec=false&loose=false&code_lz=MYewdgzgLgBBCOAbA8gVygFRAQQE64EMBPGAXhgApQwoBTGgLjilwEswBzASiYujc4wAPjDCoAtgCNauYaNSJEXANoBdNWQB8MAN4AoGDGrQYuEAHcITfuw4by1OjQB0EAA6JWUCgHoAOmA-3ADcegamtFCouGCmFhDO4gRuVCCIEmAAyiy2WrrhhsawoIhWzAJ2qmRGaRnZFa4eXr5-EABUfkKtbUFcoQURUTE1pQOGzgBmrIh0uFR5-oZLS7iR0bHAMACEpOQA5Huhy4YAvlxjMInJ86Tai8eGrBOUW6wQAHIE7xTvEtJzwC4QPyD2WqyGsV-UhkVD6F1OYVBMCelE2u327wAqgAZbF7LggpGDdbyRRHUEnRGg8Ek4Dk5ZncmMvQnYJAA&debug=false&forceAllTransforms=false&shippedProposals=false&circleciRepo=&evaluate=false&fileSize=false&timeTravel=false&sourceType=module&lineWrap=true&presets=es2015%2Ctypescript&prettier=false&targets=&version=7.11.6&externalPlugins=
+/*
+"use strict";
+
+var sqlOutToArray = function sqlOutToArray(content) {
+  var rows = content.split(/\n/g);
+  return rows.map(function (columnString) {
+    var cols = columnString.split(/\s*\|\s*/g);
+    return cols.filter(function (c) {
+      return c !== '';
+    }).map(function (c) {
+      if (!isNaN(Number(c))) {
+        return Number(c);
+      }
+
+      if (c === 'NULL') {
+        return null;
+      }
+
+      return c;
+    });
+  });
+};
+*/
